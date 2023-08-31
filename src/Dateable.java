7abed26e284e043f5579e6d7caba6adfc93bf5d2
Onvm2007 <nicks2026@student.cis.edu.hk>
2023-08-31 14:21:15 +0800

    first lab

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me your age");
+        int age = scanner.nextInt();
+        System.out.println("you should date someone who's at least " + (age/2 +7));
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("give me your age");
+        int age = scanner.nextInt();
+        Scanner words = new Scanner(System.in);
+        System.out.println("type your name");
+        String name = words.nextLine();
+        System.out.println("hello " + name + " you should read " + (100-age) + " pages of a book before you drop it");
+
 
     }
 }
